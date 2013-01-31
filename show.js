f113e3e7c5dee700c631319996c5462f8301dec6
Victor Mak <victormakcy@gmail.com>
2013-01-30 20:03:43 -0500

    fixed closure problem, have js showing random order of boxes now

--- show.js
+++ show.js
@@ -9,33 +9,25 @@ $(document).ready(function(){
 });
 
 function displayPattern(){
+	var i = 0;
 	var randNumber;
 	var gameBlockID;
+	var color;
 	level = level + 1;
-	for (var i = 0; i < (level + 2); i++){
+	var timer = setInterval(function(){
 		randNumber = Math.floor((Math.random() * 6) + 1);
 		gameBlockID = "#game-block" + randNumber;
-		switch(randNumber)
-		{
-		case 1:
-			$(gameBlockID).css("background-color", "#000");
-			break;
-		case 2:
-			$(gameBlockID).css("background-color", "#000");
-			break;
-		case 3:
-			$(gameBlockID).css("background-color", "#000");
-			break;
-		case 4:
-			$(gameBlockID).css("background-color", "#000");
-			break;
-		case 5:
-			$(gameBlockID).css("background-color", "#000");
-			break;
-		case 6:
-			$(gameBlockID).css("background-color", "#000");
-			break;
-		}
-		setTimeout(function(){$(gameBlockID).css("background-color", "#FFF")},3000);
-	};
+		color = $(gameBlockID).css("border-color");
+		$(gameBlockID).css("background-color", color);
+
+		setTimeout(function(){
+		  $(gameBlockID).css("background-color", "#FFF");
+			i = i + 1;
+			if(i == (level + 2)){
+				console.log("end");
+				clearInterval(timer);
+				return;
+			}
+		}, 2000);
+  }, 2500);
 };
