38f4c8fba697b20731c3955ce48d9ab07b03f036
Victor Mak <victormakcy@gmail.com>
2013-02-08 21:38:27 -0500

    update1

--- show.js
+++ show.js
@@ -1,10 +1,34 @@
 var level = 0;
+var playersTurn = false;
+var timer
 
 $(document).ready(function(){  
 	$("#start").click(function(){
 	  $("#play-window").css("display","block");
 	  $("#start-window").css("display","none");
 	  displayPattern();
+	});
+
+	$("#quit").click(function(){
+		level = 0;
+		playerTurn = false;
+		clearInterval(timer);
+	  $("#play-window").css("display","none");
+	  $("#start-window").css("display","block");
+	});
+
+	$(".game-block").mousedown(function(){
+		if (playersTurn == false){
+			alert("not your turn");
+		}else{
+			$(this).css("background-color", $(this).css("border-color")); 
+		}
+	});
+
+	$(".game-block").mouseup(function(){
+		if(playersTurn == true){
+			$(this).css("background-color", "#FFF"); 
+		}
 	});
 });
 
@@ -14,7 +38,7 @@ function displayPattern(){
 	var gameBlockID;
 	var color;
 	level = level + 1;
-	var timer = setInterval(function(){
+	timer = setInterval(function(){
 		randNumber = Math.floor((Math.random() * 6) + 1);
 		gameBlockID = "#game-block" + randNumber;
 		color = $(gameBlockID).css("border-color");
@@ -24,10 +48,23 @@ function displayPattern(){
 		  $(gameBlockID).css("background-color", "#FFF");
 			i = i + 1;
 			if(i == (level + 2)){
-				console.log("end");
 				clearInterval(timer);
+				playersTurn = true;
 				return;
 			}
 		}, 2000);
   }, 2500);
 };
+
+function comparePattern(){
+
+}
+
+function nextLevel(){
+
+}
+
+function gameOver(){
+
+}
+
